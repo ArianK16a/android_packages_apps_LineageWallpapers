c73f12aae3b9b8344b13f65906e881cd09e49dd7
Arian <arian.kulmer@web.de>
2020-02-02 15:40:38 +0100

    add script
    Change-Id: I8ba9fce462faa584c4e6b2f62fa9755b2e136fcf

---
+++ script.sh
@@ -0,0 +1,13 @@
+#size=576x384
+size=768x432
+convert res_1440p/common/drawable-nodpi/abstract_orderofchaos.jpg -resize $size res_1440p/common/drawable-nodpi/abstract_orderofchaos_small.jpg
+convert res_1440p/common/drawable-nodpi/nature_ice.jpg -resize $size res_1440p/common/drawable-nodpi/nature_ice_small.jpg
+convert res_1440p/common/drawable-nodpi/nature_nebula.jpg -resize $size res_1440p/common/drawable-nodpi/nature_nebula_small.jpg
+convert res_1440p/common/drawable-nodpi/nature_night.jpg -resize $size res_1440p/common/drawable-nodpi/nature_night_small.jpg
+convert res_1440p/common/drawable-nodpi/urban_dayroad.jpg -resize $size res_1440p/common/drawable-nodpi/urban_dayroad_small.jpg
+
+convert res_1440p/full/drawable-nodpi/abstract_ink.jpg -resize $size res_1440p/full/drawable-nodpi/abstract_ink_small.jpg
+convert res_1440p/full/drawable-nodpi/abstract_spacestory.jpg -resize $size res_1440p/full/drawable-nodpi/abstract_spacestory_small.jpg
+convert res_1440p/full/drawable-nodpi/nature_sea.jpg -resize $size res_1440p/full/drawable-nodpi/nature_sea_small.jpg
+convert res_1440p/full/drawable-nodpi/space_riseup.jpg -resize $size res_1440p/full/drawable-nodpi/space_riseup_small.jpg
+convert res_1440p/full/drawable-nodpi/urban_nightroad.jpg -resize $size res_1440p/full/drawable-nodpi/urban_nightroad_small.jpg
